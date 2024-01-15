fdead3140e8c1e90dcf6ac7324595901c513cb59
Your Name <louisginting54@gmail.com>
2024-01-16 01:15:08 +0700

    add function enterResult

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
-function calculate(event,sideLength, result){
+function calculate(sideLength, result){
     var sisi = document.getElementById(sideLength).value;
     var result = document.getElementById(result);
     
     if (sideLength === 'sisi'){
         if (sisi !== ''){
-            var luas = sisi * sisi;
-            result.innerHTML = "L = S x S <br>" + "L = " + sisi + "*"+ sisi + "<br>" + "L = " + luas;
+            var luas = parseInt(sisi) * parseInt(sisi);
+            result.innerHTML = "L = S x S <br>" + "L = " + sisi + "x"+ sisi + "<br>" + "L = " + luas;
         }
         else{
             window.alert("Please Input the Side Length");
@@ -14,8 +14,8 @@ function calculate(event,sideLength, result){
     }
     else if (sideLength === 'sisiKeliling'){
         if (sisi !== ''){
-            var keliling = 4 * sisi;
-            result.innerHTML = "L = 4 x S <br>" + "L = " + "4" + "*" + sisi + "<br>" + "L = " + keliling;
+            var keliling = 4 * parseInt(sisi);
+            result.innerHTML = "L = 4 x S <br>" + "L = " + "4" + "x" + sisi + "<br>" + "L = " + keliling;
         }
         else{
             window.alert("Please Input the Side Length");
@@ -23,6 +23,15 @@ function calculate(event,sideLength, result){
         }
     }
 }
+
+function enterResult(event, sideLength, result){
+    if(event.key === 'Enter'){
+        result.innerHTML = sisi === '' ? window.alert("Please Input the Side Length") : calculate(sideLength, result);
+    }
+    
+    
+}
+
 function reset(sideLength, result){
     document.getElementById(sideLength).value = '';
     document.getElementById(result).innerHTML = '';
